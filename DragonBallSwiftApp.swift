d57d47da6c7ef123b4d60229f19c5891f25a51a3
kontroldev <raulgallego79@icloud.com>
2024-05-28 22:08:20 +0200

    Añadido proyecto y actualización README

---
+++ DragonBallSwiftApp.swift
@@ -0,0 +1,17 @@
+//
+//  DragonBallSwiftApp.swift
+//  DragonBallSwift
+//
+//  Created by Raúl Gallego Alonso on 28/5/24.
+//
+
+import SwiftUI
+
+@main
+struct DragonBallSwiftApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
